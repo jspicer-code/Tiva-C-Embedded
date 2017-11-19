9d3d6d75a46447ce0b9a34b47f4ec85b0f522c33
jspicer-ltu <31192375+jspicer-ltu@users.noreply.github.com>
2017-11-18 19:13:22 -0500

    Added Experiement12-SPI-MultiplexedDisplay
    Also removed sample circuit1 diagrams from previous projects.

---
+++ Experiment12-SPI-MultiplexedDisplay/Display.h
@@ -0,0 +1,17 @@
+// File:  Display.h
+// Author: JS
+// Date:  10/28/17
+// Purpose: Multiplexed display service module.
+// Hardware:  TM4C123 Tiva board
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+
+void Display_Initialize(void);
+void Display_Update(uint32_t value);
+void Display_Blank(void);
+
+#endif
+
